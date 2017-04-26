845b6fdfe43559332bb6aff63e5916fa392c47b8
Tai Hovanky <thovanky@arrivelogistics.local>
2017-04-25 22:32:39 -0500

    functioning MVC API, starting on UI

---
+++ Todo.UI/Index.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World! This is what I have to do today:</h1>
+            </div>
+        );
+    }
+}
+
+App.displayName = "App";
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ Todo.UI/webpack.config.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    }
+};
